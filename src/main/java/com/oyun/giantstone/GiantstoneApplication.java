3040faa214a352441dfd1911baa6a4bcde6f525b
Changzhen <chzhlp@163.com>
2018-06-13 17:35:39 +0800

    init

---
+++ src/main/java/com/oyun/giantstone/GiantstoneApplication.java
@@ -0,0 +1,12 @@
+package com.oyun.giantstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiantstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiantstoneApplication.class, args);
+	}
+}
